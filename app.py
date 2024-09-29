fcadad83d782e1e284ad711f99e7c6229ebabf48
Wasay Rizwan <wasayr1@bu.edu>
2024-09-29 03:34:31 -0400

    flask initial check

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import numpy as np
+from kmeans import KMeans
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/kmeans', methods=['POST'])
+def run_kmeans():
+    data = request.json
+    X = np.array(data['points'])
+    n_clusters = data['n_clusters']
+    
+    kmeans = KMeans(n_clusters=n_clusters)
+    clusters = kmeans.fit(X)
+    
+    return jsonify({
+        'clusters': clusters.tolist(),
+        'centroids': kmeans.centroids.tolist()
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)
